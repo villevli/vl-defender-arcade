1a13abc292af5149007c962b657539a3a71e73a4
Ville Lindblad <villevli.dev@gmail.com>
2025-06-19 15:59:10 +0300

    Make player hit enemies more reliably

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -9,13 +9,14 @@ namespace VLDefenderArcade
     public class PlayerProjectile : MonoBehaviour
     {
         [SerializeField]
-        private float _speed = 10.0f;
+        private float _speed = 30.0f;
         [SerializeField]
         private float _lifetime = 1.0f;
 
         private Map _map;
         private float _inheritedSpeed = 0;
         private float _elapsed = 0;
+        private Enemy _hitEnemy;
 
         public void InheritSpeed(float speed)
         {
@@ -31,6 +32,7 @@ namespace VLDefenderArcade
         {
             _elapsed = 0;
             _inheritedSpeed = 0;
+            _hitEnemy = null;
         }
 
         private void OnDisable()
@@ -42,22 +44,33 @@ namespace VLDefenderArcade
 
         private void Update()
         {
-            transform.Translate((_speed + _inheritedSpeed) * Time.deltaTime, 0, 0);
+            // Hit effect after raycast is delayed by one frame so we get to render one frame with the projectile hitting
+            if (_hitEnemy)
+            {
+                _hitEnemy.Kill();
+                _hitEnemy = null;
+                // projectile gets destroyed
+                GameObjectPool.Destroy(gameObject);
+                return;
+            }
+
+            var pos = (Vector2)transform.position;
+            var dir = (Vector2)transform.right;
+            var deltaLength = (_speed + _inheritedSpeed) * Time.deltaTime;
+
+            // Using raycast instead of having collider on the projectile for more reliable hits when moving fast
+            var hit = Physics2D.Raycast(pos, dir, deltaLength);
+            if (hit && hit.transform.TryGetComponent<Enemy>(out var enemy))
+            {
+                _hitEnemy = enemy;
+                deltaLength = Mathf.Min(deltaLength, hit.distance + 0.1f);
+            }
+
+            transform.position = pos + dir * deltaLength;
 
             _elapsed += Time.deltaTime;
             if (_elapsed >= _lifetime)
             {
-                GameObjectPool.Destroy(gameObject);
-            }
-        }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            // Debug.Log($"{name} OnTriggerEnter2D {other.name}");
-            if (other.TryGetComponent<Enemy>(out var enemy))
-            {
-                enemy.Kill();
-                // projectile gets destroyed
                 GameObjectPool.Destroy(gameObject);
             }
         }
